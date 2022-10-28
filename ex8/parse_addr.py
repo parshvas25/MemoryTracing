c1bf9390cc94549141bf7ae5258a1d1401ca2c78
parshvas25 <parshvashah414@gmail.com>
2022-10-28 15:14:04 -0400

    Add parser

---
+++ ex8/parse_addr.py
@@ -0,0 +1,27 @@
+# INPUT
+# S 0x7ff0008e8,1
+# I  0x400590,4
+# L  0x601020,6
+# I  0x400596,8
+# S 0x7ff0008e0,2
+# M  0x4fdf720,6
+import sys
+def parse(fName):
+    instructions = []
+    data = []
+    f = open(fName, "r")
+    for line in fp:
+        #data = line.strip().split()
+       # ins = data[0]
+        # addr = data[1]
+        print(line)
+        
+
+
+            
+
+    
+
+    return None 
+
+parse().argv
